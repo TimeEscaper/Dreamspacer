c708be209e55443ea896f7da636792466201430a
TimeEscaper <rentgeny05@gmail.com>
2016-02-02 22:44:43 +0300

    Dialogs downloading started.

--- main.cpp
+++ main.cpp
@@ -5,13 +5,26 @@
 #include "debug.h"
 #include <fstream>
 #include <QApplication>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QObject>
+#include <QEventLoop>
 using namespace std;
+
+void GetDialogFinished(QNetworkReply* res)
+{
+    Debug::DebugMessage(res->readAll());
+}
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    w.ui->listWidget->setVisible(false);
 
     Core::Sec::UserSession User;
     ifstream input("udf.dat",ios::binary);
@@ -22,6 +35,21 @@ int main(int argc, char *argv[])
         Core::Global::token = User.token;
         Core::Global::masterId = User.userId;
         w.ui->webView->setVisible(false);
+        w.ui->listWidget->setVisible(true);
+
+        QNetworkAccessManager downloader;
+        QNetworkRequest getRequest;
+        getRequest.setUrl(QUrl("https://api.vk.com/method/messages.getDialogs?count=1&access_token="+Core::Global::token));
+        QNetworkReply *res;
+       // QObject::connect(res,SIGNAL(finished()),this,SLOT(GetDialogFinished()));
+        res = downloader.get(getRequest);
+
+        QEventLoop ev;
+        QObject::connect(&downloader,SIGNAL(finished(QNetworkReply*)),&ev,SLOT(quit()));
+        ev.exec();
+
+        Debug::DebugMessage(res->readAll());
+
     }
     else
     {
